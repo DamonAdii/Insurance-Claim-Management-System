727bc27ea529cbd22afdfa53af554c3a6e8e41e0
DamonAdii <chauhan775386@gmail.com>
2025-08-12 22:12:49 +0530

    Final Commit

--- src/main/java/com/icms/repository/ClaimRepository.java
+++ src/main/java/com/icms/repository/ClaimRepository.java
@@ -9,4 +9,5 @@ import java.util.Optional;
 @Repository
 public interface ClaimRepository extends JpaRepository<Claim,Long> {
     Optional<Claim> findByClaimNumber(String claimNumber);
+    boolean existsByClaimNumber(String claimNumber);
 }
