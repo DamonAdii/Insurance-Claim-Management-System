c6a6ef7aff8152aafc91a0c0a2a51fd8b1d61f17
DamonAdii <chauhan775386@gmail.com>
2025-08-12 19:54:44 +0530

    first icms commit

---
+++ src/main/java/com/icms/InsuranceClaimManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.icms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsuranceClaimManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsuranceClaimManagementSystemApplication.class, args);
+	}
+
+}
